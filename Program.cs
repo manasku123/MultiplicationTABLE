d8b43e4bd644dd0429aefbc39c67a9df218b0b17
Manas Kumar Biswal <134088923+manasku123@users.noreply.github.com>
2024-03-28 21:35:31 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+namespace MultiplicationApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Type a number: ");
+            int number =Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("The multiplication table of number "+number+" is: ");
+            if (number>0 && number <= 100)
+            {
+                for (int i= 1; i <= 10; i++)
+                {
+                    Console.WriteLine(number +"*"+ i +"="+ number*i);
+                };
+            }
+            else
+            {
+                Console.WriteLine("Not Found");
+            }
+        }
+    }
+}
